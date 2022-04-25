a786be595801e000d33f594dc35a46c7362035fa
MazinoXX <o.tru08@gmail.com>
2022-04-25 11:01:19 -0500

    View: Creando un usuario con propiedades validas

--- test/views/UserViews.test.js
+++ test/views/UserViews.test.js
@@ -9,5 +9,26 @@ describe("Test for UserViews", () => {
         expect(result.error).toMatch(/payload no exist/)
 
     })
-    
+    test('Return an error object when to try to create a new user with a payload with invalid properties', () => {
+        const payload = {username: null, name: 12, id: "id"}
+        const result = UserView.createUser(payload)
+
+        expect(result.error).toMatch(/necesitas tener un valor valido/)
+    })
+    test('Return an error object when to try to create a new user with a payload with missing properties', () => {
+        const payload = {username: "username"}
+        const result = UserView.createUser(payload)
+
+        expect(result.error).toMatch(/necesitas tener un valor valido/)
+    })
+    test('Create a user by a given valid payload', () => {
+        const payload = {username: "omartrujillo", name: "omar", id: 1}
+        const result = UserView.createUser(payload)
+
+        expect(result.name).toBe("omar")
+        expect(result.username).toBe("omartrujillo")
+        expect(result.id).toBe(1)
+
+    })
+
 })
